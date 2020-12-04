d80388f84be9f80b5a867fc7c92ef5c3c4d3de01
DDstar <chunmeiwu@mie.com>
2020-12-05 00:02:18 +0800

    开发环境去掉证书验证

--- lib/utils/dio_util.dart
+++ lib/utils/dio_util.dart
@@ -51,15 +51,15 @@ class DioUtil {
     _dio.options.headers = headers;
 
     // 在调试模式下需要抓包调试，所以我们使用代理，并禁用HTTPS证书校验
-   // (_dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate =
-   //     (client) {
+   (_dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate =
+       (client) {
    //   client.findProxy = (uri) {
    //     //return "PROXY 192.168.2.102:8888";
    //     return "PROXY 192.168.1.6:8888";
    //   };
    //   //代理工具会提供一个抓包的自签名证书，会通不过证书校验，所以我们禁用证书校验
-   //   client.badCertificateCallback = (X509Certificate cert, String host, int port) => true;
-   // };
+     client.badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+   };
 
     return _dio;
   }
@@ -67,6 +67,7 @@ class DioUtil {
   // 校验服务器返回结果，统一处理
   static bool checkRequestResult(responseData, {showToast = false}) {
     if (responseData == null) {
+      print("responseData==null");
       if (showToast) { BotToast.showText(text: "请求失败"); }
       return false;
     } else {
@@ -102,6 +103,7 @@ class DioUtil {
       }else{
         response = await dio.request(url, data: parameters, options: Options(method: method),);
       }
+      print("code=${response.statusCode}\n");
       var responseData = response.data;
       if (responseData is String) {
         result = convert.jsonDecode(responseData);
